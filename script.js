e541d2da7e9949e686d01ae4f1ad3e196941f2fb
quandaworld <46980777+quandaworld@users.noreply.github.com>
2023-09-29 21:47:01 -0700

    Add basic math operators functions

---
+++ script.js
@@ -0,0 +1,32 @@
+let firstNum = 0;
+let secondNum = 0;
+let operator = '';
+
+const add = (a, b) => {
+  return a + b;
+};
+
+const subtract = (a, b) => {
+  return a - b;
+};
+
+const multiply = (a, b) => {
+  return a * b;
+};
+
+const divide = (a, b) => {
+  return a / b;
+};
+
+const operate = (operator, firstNum, secondNum) => {
+  switch(operator) {
+    case '+':
+      return add(firstNum, secondNum);
+    case '-':
+      return subtract(firstNum, secondNum);
+    case '*':
+      return multiply(firstNum, secondNum);
+    case '/':
+      return divide(firstNum, secondNum);
+  }
+};
