f4f259a50c26c7cd7cc27e6824df295d9f63438e
quandaworld <46980777+quandaworld@users.noreply.github.com>
2023-10-08 23:54:06 -0700

    Update JS

--- script.js
+++ script.js
@@ -5,51 +5,82 @@ const current = document.getElementById('current');
 const ac_button = document.getElementById('ac');
 const ce_button = document.getElementById('ce');
 const percent_button = document.getElementById('percent');
+const decimal_button = document.getElementById('decimal');
 const negative_button = document.getElementById('negative');
 const equal_button = document.getElementById('equal');
 let firstNum = 0;
 let secondNum = 0;
 let operator = '';
 
-numbers.forEach(button => {
-  button.addEventListener('click', () => {
-    entry.textContent += button.textContent;
-    current.textContent += button.textContent;
-  });
-});
+function appendNumbers(e) {
+  if (e.type === 'click') entry.innerText += `${e.target.innerText}`;
+  if (e.type === 'keydown') entry.innerText += `${e.key}`;
+}
 
-operators.forEach(button => {
-  button.addEventListener('click', () => {
-    if (entry.textContent === '') entry.textContent = '0';
-    if (Number(entry.textContent[entry.textContent.length - 1])) {
-      entry.textContent += ` ${button.textContent} `;
-    }
-    current.textContent = '';
-  });
-});
-
-ac_button.addEventListener('click', () => {
-  location.reload(); // will be updated to reset all variables
-});
-
-ce_button.addEventListener('click', () => {
-  entry.textContent = entry.textContent.slice(0, entry.textContent.length - 1);
-});
-
-// percent_button.addEventListener('click', () => {
-//   entry.textContent = '';
-// });
-
-negative_button.addEventListener('click', () => {
-  const lastEntry = entry.textContent[entry.textContent.length - 1];
-  if (Number(lastEntry)) {
-    entry.textContent = entry.textContent.slice(0, entry.textContent.length - 1) + `-${lastEntry}`;
+function appendOperators(e) {
+  if (e.type === 'click') entry.innerText += ` ${e.target.innerText}`;
+  if (e.type === 'keydown') {
+    if (e.key === '+') entry.innerText += ' +';
+    if (e.key === '-') entry.innerText += ' −';
+    if (e.key === '*') entry.innerText += ' ×';
+    if (e.key === '/') entry.innerText += ' ÷';
   }
-});
+}
 
-equal_button.addEventListener('click', () => {
-  entry.textContent = '';
-});
+function appendPercent() {
+  if (e.type === 'click') entry.innerText += ` ${e.target.innerText}`;
+  if (e.type === 'keydown') entry.innerText += ` ${e.key}`;
+}
+
+function appendDecimal() {
+  // if (e.type === 'click') entry.innerText += ` ${e.target.innerText}`;
+  // if (e.type === 'keydown') entry.innerText += ` ${e.key}`;
+}
+
+function appendNegativeSign() {
+  // if (e.type === 'click') entry.innerText += ` ${e.target.innerText}`;
+  // if (e.type === 'keydown') entry.innerText += ` ${e.key}`;
+}
+
+function appendEqual() {
+  // if (e.type === 'click') entry.innerText += ` ${e.target.innerText}`;
+  // if (e.type === 'keydown') entry.innerText += ` ${e.key}`;
+}
+
+function clearEntry() {
+  entry.innerText = entry.innerText.slice(0, entry.innerText.length - 1);
+}
+
+function clearAllEntry() {
+  location.reload(); // will be updated to reset all variables
+}
+
+function handleKeyboardInput(e) {
+  if (e.key >= 0 && e.key <= 9) appendNumbers(e);
+  if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/') appendOperators(e);
+  if (e.key === '%') appendPercent();
+  if (e.key === 'Backspace') clearEntry();
+  if (e.key === 'Escape') clearAllEntry();
+  console.log(e);
+}
+
+window.addEventListener('keydown', handleKeyboardInput);
+
+numbers.forEach(button => button.addEventListener('click', appendNumbers));
+
+operators.forEach(button => button.addEventListener('click', appendOperators));
+
+ce_button.addEventListener('click', clearEntry);
+
+ac_button.addEventListener('click', clearAllEntry);
+
+percent_button.addEventListener('click', appendPercent);
+
+decimal_button.addEventListener('click', appendDecimal);
+
+negative_button.addEventListener('click', appendNegativeSign);
+
+equal_button.addEventListener('click', appendEqual);
 
 const add = (a, b) => {
   return a + b;
